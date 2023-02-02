a35170d753f2a6f7d28e683c296e6649deb00ef9
Kairat <burambaevkairat@mail.ru>
2023-02-02 12:17:57 +0300

    task a completed

--- idt.py
+++ idt.py
@@ -1,14 +1,25 @@
-import tempfile
+import pydicom as pm
+import os
 
-import pydicom
-from pydicom.data import get_testdata_file
+from glob import glob
 
-filename = get_testdata_file('0a67f9edb4915467ac16a565955898d3')
-dataset = pydicom.dcmread(filename)
+path_to_dicom = 'src/*' # path to initial dicom file
 
-data_elements = ['PatientID',
-                 'PatientBirthDate']
-for de in data_elements:
-    print(dataset.data_element(de))
+def anonymize_dicom_and_save_in_new_structure(in_path, patient_name='anonymoys'):
+    file_dicom = pm.dcmread(in_path)
+    file_dicom.PatientName = patient_name
+    # file anonymization
 
+    new_path = f'tree/{file_dicom.StudyInstanceUID}/{file_dicom.SeriesInstanceUID}'
+    os.makedirs(new_path, exist_ok="True")
+    out_path = f'{new_path}/{file_dicom.SOPInstanceUID}.dcm'
+    # creating an out path
 
+    file_dicom.save_as(out_path)
+
+    with open('description_of_the_new_structure.txt', 'a') as f:
+        f.writelines(f'{in_path} --> {out_path}\n')
+    # create file description change file path
+
+for file in glob(path_to_dicom):
+    anonymize_dicom_and_save_in_new_structure(file)
